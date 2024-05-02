d7d763b901ed5020df072b2720f998d4afad7c6e
Rishabh Tiwari <rishtiwari98@gmail.com>
2024-05-02 15:15:31 +0200

    Added custom retriever

--- Examples/Read_Text_Doc/main.py
+++ Examples/Read_Text_Doc/main.py
@@ -1,6 +1,7 @@
 from langchain.document_loaders import TextLoader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -9,13 +10,8 @@ embeddings = OpenAIEmbeddings()
 
 text_splitter = CharacterTextSplitter(
     separator="\n",
-    chunk_size=200,  # It will first select 200 characters from the text and then split it by the separator
-                    # This is useful when the text is too long and we want to split it into smaller chunks for processing
-    chunk_overlap=100  # The number of characters to overlap between chunks so it goes back the last 100 characters and then split
-                    # This is useful when the separator is a sentence and we want to make sure that the sentence is 
-                    # not split between two chunks. If there are more than one sentence in this overlap, it will split. 
-                    # If there are more than one sentence in this overlap, it will split. Otherwise, it will continue 
-                    # to take chunks until it finds a separator.
+    chunk_size=200,
+    chunk_overlap=0
 )
 
 loader = TextLoader("facts.txt")
@@ -23,6 +19,16 @@ docs = loader.load_and_split(
     text_splitter=text_splitter
 )
 
-for doc in docs:
-    print(doc.page_content)
+db = Chroma.from_documents(
+    docs,
+    embedding=embeddings,
+    persist_directory="emb"
+)
+
+results = db.similarity_search(
+    "What is an interesting fact about the English language?"
+)
+
+for result in results:
     print("\n")
+    print(result.page_content)
